de4df06a6c0924ef2d691b922a680c82a30dcc16
jonjongames1997 <87885521+jonjongames1997@users.noreply.github.com>
2024-10-17 23:47:39 -0400

    preparing for 1.8 update

--- SimplePedMenu/SimplePedMenu.cs
+++ SimplePedMenu/SimplePedMenu.cs
@@ -1658,7 +1658,7 @@ public class SimplePedMenu : Script
     public SimplePedMenu()
     {
         this._menuPool = new MenuPool();
-        UIMenu mainMenu = new UIMenu("~o~Simple Ped Menu", "~b~Mod ~g~by~w~ JonJonGames ~y~v1.7");
+        UIMenu mainMenu = new UIMenu("~o~Simple Ped Menu", "~b~Mod ~g~by~w~ JonJonGames ~y~v1.8");
         this._menuPool.Add(mainMenu);
         this.PlayerModelMenu(mainMenu);
         this.VehicleMenu(mainMenu);
